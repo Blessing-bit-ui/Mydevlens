eaf5b37c4e72f39c5ee25985563661f8fb751219
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-20 13:12:33 -0700

    created the extension array

--- my-Devlens/src/App.jsx
+++ my-Devlens/src/App.jsx
@@ -1,3 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import console from "./assets/console.png";
+import ExtensList from './ExtensList'
+
+
+ function App(){
+    return (
+      <>
+        <h1>hhhh</h1>
+      </>
+    );
+}
+export default App
 
---
+++ my-Devlens/src/ExtensList.jsx
@@ -0,0 +1,87 @@
+import devlens from './assets/devlens.png';
+import css from './assets/css.png';
+import speed from './assets/speed.jpg';
+import json from './assets/json.png';
+import tab from './assets/tab.jpg';
+import view from './assets/view.png';
+import mark from './assets/mark.jpg';
+import grid from './assets/grid.png';
+import palette from './assets/palette.png';
+import link from './assets/link.png';
+import dom from './assets/dom.jpg';
+import console from './assets/console.png'
+
+const Extensions =[
+    {
+        name:"DevLens",
+        use: "Quickly inspect page layouts and visualise elements boundaries",
+        photoName:"assets/devlens.png",
+        status:"active"
+    },
+    {
+        name: "StlyeSpy",
+        use:"Instantly analyse and copy Css from any webpage element",
+        photoName:"assets/css.png",
+        status:"active",
+    },
+    {
+      name:"SpeedBoost",
+      use:"Optimizes browser resource usage to accelerate page loading.",
+      photoName:"assets/speed.jng",
+      status:"inactive"
+    },
+    {
+        name:" JSONWizard",
+        use:"Formats, validate and prettifies JSON responses in-browers",
+        photoName:"assets/json.png",
+        status:"active",
+    },
+    {
+        name:" TabMaster Pro",
+        use:"Organises browers tabs into groups and sessions",
+        photoName:"assets/tab.jpg",
+        status:"active"
+    },
+    {
+        name:"View PortBuddy",
+        use:"simulates various screen resolutions directly within the browser",
+        photoName:"assets/view.png",
+        status:"inactive"
+    },
+    {
+        name:"Markup Notes",
+        use:"Enables annotation and notes directly onto webpages for collaboratives debugging",
+        photoName:"assets/mark.jpg",
+        status:"active"
+    },
+    {
+        name:"GridGuides",
+        use:"Overlay customize grids and alignment guides to any webpage.",
+        photoName:"assets/grid.png",
+        status:"inactive"
+    },
+    {
+      name:"Paletter Picker",
+      use:"Instantly extracts color palette from webpages.",
+      photoName:"assets/palette.png",
+      status:"inactive" 
+    },
+    {
+        name:"LinkerChecker",
+        use:"Scans and highlights broken on any page",
+        photoName:"assets/link.png",
+        status:"active"
+    },
+    {
+        name:"DOM Snapchat",
+        use:"Capture and export Dom structures quickly",
+        photoName:"assets/dom.jpg",
+        status:"inactive"
+    },
+    {
+        name:"Console Plus",
+        use:"Enhance developer console with advance filter and logging",
+        photoName:"assets/console.png",
+        status:"active"
+    }
+]
